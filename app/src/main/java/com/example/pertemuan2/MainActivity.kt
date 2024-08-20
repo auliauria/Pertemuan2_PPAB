b405d9c828f91c691030c98542cd09e42cd20489
auliauria <arhm2610@gmail.com>
2024-08-20 19:09:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.pertemuan2
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pertemuan2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btnCount.setOnClickListener{
+                Toast.makeText(this@MainActivity, edtUsername.text, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
